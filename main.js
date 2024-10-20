f46fae04dd85c587aebda1307789bccdb005a15a
unknown <mbmtms@nver.com>
2024-10-20 18:17:21 +0900

    1st

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+function lightyellow() {
+  document.body.style.backgroundColor = 'lightyellow'
+}
+function reset() {
+  document.body.style.backgroundColor = 'white'
+}
